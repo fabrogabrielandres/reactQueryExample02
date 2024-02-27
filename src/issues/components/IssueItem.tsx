4603d23df183f47b98a382d75ae39d143bac0542
GABIACER\fabro <>
2024-02-27 13:18:20 +0100

    .

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -18,11 +18,19 @@ export const IssueItem:FC<Props> = ({issue}) => {
         queryClient.prefetchQuery({queryKey:['issue',issueNumber,"comments"],queryFn:()=>getIssueComments(issueNumber)})
     };
     const preSetData = (issueNumber:number)=>{
-        queryClient.setQueryData(['issue',issueNumber],getIssueInfo(issueNumber))
+        queryClient.setQueryData(['issue',issueNumber],issue)
+
     };
+    //el prefetch carga el issue mediante un fetch (getIssueInfo) y los comentarios de los issue con otro fetch(getIssueComments),
+    //el pre set ya carga lo que seria el getIssueInfo porque es la misma informacion ya la poseo , por eso cuando entras a un issue rapido antes de los dos segundo se ve algo informacion del issue y luego carga los comentarios
+     
     
     return (
-        <div className="card mb-2 issue" onClick={()=>navigate(`/issues/issue/${issue.number}`)} onMouseEnter={()=>preSetData(issue.number)}>
+        <div className="card mb-2 issue" onClick={()=>navigate(`/issues/issue/${issue.number}`)}
+             onMouseEnter={()=>preSetData(issue.number)}
+            //  onMouseEnter={()=>prefetchData(issue.number)}
+             
+        >
             <div className="card-body d-flex align-items-center">
             {issue.state  == State.Close ?
                 <FiInfo size={30} color="red" /> : <FiCheckCircle size={30} color="green" />
