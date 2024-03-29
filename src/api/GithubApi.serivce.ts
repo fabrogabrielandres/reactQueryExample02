45ccc3b655ee553dfee7bc133542bc4e531934a8
GABIACER\fabro <gabriel.fabro@pirani.co>
2024-03-29 15:42:56 +0100

    new tokken

--- src/api/GithubApi.serivce.ts
+++ src/api/GithubApi.serivce.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const GithubApi = axios.create({
     baseURL: "https://api.github.com/repos/facebook/react",
     headers: {
-        Authorization: "Bearer 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"
+        Authorization: "Bearer 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"
     }
 })
 
